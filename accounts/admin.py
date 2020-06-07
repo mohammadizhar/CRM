24894d374456d0933cc78f251309d775f83fd315
mohammadizhar <10izhar99@gmail.com>
2020-06-08 01:12:56 +0530

    part 7 complete

--- accounts/admin.py
+++ accounts/admin.py
@@ -5,4 +5,5 @@ from .models import *
 admin.site.register(Customer)
 admin.site.register(Product)
 admin.site.register(Order)
+admin.site.register(Tag)
 
--- accounts/models.py
+++ accounts/models.py
@@ -9,6 +9,11 @@ class Customer(models.Model):
 
     def __str__(self):
         return self.name
+class Tag(models.Model):
+    name=models.CharField(max_length=200,null=True)
+    
+    def __str__(self):
+        return self.name
 
 class Product(models.Model):
     STATUS=(
@@ -18,14 +23,20 @@ class Product(models.Model):
     category=models.CharField(max_length=200,null=True,choices=STATUS)
     description=models.CharField(max_length=200,null=True)
     date_created=date_created=models.DateTimeField(auto_now_add=True)
+    tags=models.ManyToManyField(Tag)
+    def __str__(self):
+        return self.name
+
 
 class Order(models.Model):
     STATUS=(
         ('Pending','Pending'),('Out For Delivery','Out For Delivery'),('Delivered','Delivered'))
-    #customer
-    #product
+    customer=models.ForeignKey(Customer,null=True,on_delete=models.SET_NULL)
+    product=models.ForeignKey(Product,null=True,on_delete=models.SET_NULL)
+    
     date_created=models.DateTimeField(auto_now_add=True,null=True)
     status=models.CharField(max_length=200,null=True,choices=STATUS)
+    
 
 
 
